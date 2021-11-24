87cffca254530d94b9aa65c88b7022989a61fa79
Isaac Maupin <isaac@boardvitals.com>
2021-11-24 09:54:34 -0500

    added ExpenseItem Component and styles

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,14 @@
+import './ExpenseItem.css'
+const ExpenseItem = () => {
+  return (
+    <div className='expense-item'>
+      <div>March, 28 2021</div>
+      <div className='expense-item__description'>
+        <h2>Car Insurance</h2>
+        <div className='expense-item__price'>$300.97</div>
+      </div>
+    </div>
+  )
+}
+
+export default ExpenseItem
